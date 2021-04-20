4ad58cddc3a4800aaf6cec27b1b19742e906bb58
Akula Sai Sridhar <sridhar.akula001@gmail.com>
2021-04-20 11:00:10 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const dbPath = path.join(__dirname, "cricketTeam.db");
+
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbPath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (err) {
+    console.log(`DB Error: ${err.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getBooksQuery = `
+    SELECT
+      *
+    FROM
+      book
+    ORDER BY
+      book_id;`;
+  const booksArray = await db.all(getBooksQuery);
+  response.send(booksArray);
+});
