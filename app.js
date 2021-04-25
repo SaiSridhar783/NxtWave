88371c326ed3ba75008f56ce82de0332813c8d3c
Akula Sai Sridhar <sridhar.akula001@gmail.com>
2021-04-25 10:00:12 +0000

    All APIs added and checked

--- app.js
+++ app.js
@@ -100,4 +100,101 @@ app.get("/states/:stateId", authenticateToken, async (request, response) => {
   response.send(res);
 });
 
+app.post("/districts/", authenticateToken, async (request, response) => {
+  const { districtName, stateId, cases, cured, active, deaths } = request.body;
+  const query = `
+    INSERT 
+        into district (district_name, state_id, cases, cured, active, deaths)
+    VALUES
+    (
+        '${districtName}',
+        ${stateId},
+        ${cases},
+        ${cured},
+        ${active},
+        ${deaths}
+    );
+
+    `;
+
+  await db.run(query);
+  response.send("District Successfully Added");
+});
+
+app.get(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const res = await db.get(
+      `SELECT * FROM district where district_id='${districtId}';`
+    );
+    response.send(res);
+  }
+);
+
+app.delete(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    await db.run(`DELETE FROM district where district_id='${districtId}';`);
+    response.send("District Removed");
+  }
+);
+
+app.put(
+  "/districts/:districtId/",
+  authenticateToken,
+  async (request, response) => {
+    const { districtId } = request.params;
+    const {
+      districtName,
+      stateId,
+      cases,
+      cured,
+      active,
+      deaths,
+    } = request.body;
+
+    await db.run(`
+  UPDATE 
+    district 
+  SET
+    district_name='${districtName}',
+    state_id=${stateId},
+    cases=${cases},
+    cured=${cured},
+    active=${active},
+    deaths=${deaths}
+  WHERE district_id='${districtId}';`);
+    response.send("District Details Updated");
+  }
+);
+
+app.get(
+  "/states/:stateId/stats/",
+  authenticateToken,
+  async (request, response) => {
+    const { stateId } = request.params;
+    const getStateStatsQuery = `
+    SELECT
+      SUM(cases),
+      SUM(cured),
+      SUM(active),
+      SUM(deaths)
+    FROM
+      district
+    WHERE
+      state_id=${stateId};`;
+    const stats = await database.get(getStateStatsQuery);
+    response.send({
+      totalCases: stats["SUM(cases)"],
+      totalCured: stats["SUM(cured)"],
+      totalActive: stats["SUM(active)"],
+      totalDeaths: stats["SUM(deaths)"],
+    });
+  }
+);
+
 module.exports = app;
